207b311ef1b691f5925e0d4f1adca9c22742621e
joeydarwin <joey.d@payram.com>
2025-02-10 12:04:04 +0530

    added another route which greets people

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello World" });
 });
 
+app.get("/gm", (req: Request, res: Response) => {
+  res.json({ message: "Hello good morning peeps" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
